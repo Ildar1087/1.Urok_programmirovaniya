ac9f5c2ab278bc863416296b3763e89fb80784d3
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-01-28 13:02:17 +0300

    napisana programma i ona rabotaet

---
+++ exampless008_IntroMetod/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int a1 = 23;
+int b1 = 32;
+int c1 = 74;
+int a2 = 33;
+int b2 = 05;
+int c2 = 39;
+int a3 = 99;
+int b3 = 41;
+int c3 = 73;
+
+int max = a1;
+
+if (b1 > max) max = a1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
